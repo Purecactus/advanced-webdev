3ad6471c72947c0382e923eae13c7a2b78d5ed1e
Purecactus <zaha23ct@student.ju.se>
2026-02-02 15:14:34 +0100

    started 2p1

---
+++ 2p1/Task1.js
@@ -0,0 +1,7 @@
+/*
+Objective: Use parentElement to navigate up the DOM tree.
+
+Task:
+    1. Create a <section> containing a <div> and a <p> .
+    2. Write JavaScript to select the <p> and log its parent <div> to the console.
+*/
